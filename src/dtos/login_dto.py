fa3d937069d051179719d1c089bd23a481eb7402
Andres Arcila <andresarcila2@me.com>
2025-10-30 17:44:21 -0500

    fixes

--- src/dtos/login_dto.py
+++ src/dtos/login_dto.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel;
+
 class login_dto(BaseModel):
     email: str
     password: str   
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIrouter;
+from fastapi import APIRouter;
 from src.dtos.login_dto import login_dto;
-auth_router = APIrouter();
+auth_router = APIRouter();
 
 @auth_router.post("/login")
 async def login(dto: login_dto):
